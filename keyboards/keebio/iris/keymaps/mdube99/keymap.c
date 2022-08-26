1ce8b1ddeaebd8d14dc7646f5f7e737afac6ec44
Mark Dube <mjdube99@gmail.com>
2022-08-26 18:18:39 -0500

    moved TMUX_LEADER to esc spot, moved CMD_TAB to tab location, moved SELWORD, added custom copy/paste/cut bindings

--- keyboards/keebio/iris/keymaps/mdube99/keymap.c
+++ keyboards/keebio/iris/keymaps/mdube99/keymap.c
@@ -11,6 +11,11 @@
 
 #define HOME_Z  LGUI_T(KC_T)
 #define HOME_SL RGUI_T(KC_SLSH)
+#define U_RDO C(KC_Y)
+#define U_PST C(KC_V)
+#define U_CPY C(KC_C)
+#define U_CUT C(KC_X)
+#define U_UND C(KC_Z)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -22,6 +27,7 @@ enum custom_keycodes {
   SELWORD,
   CMD_TAB,
   TMUX_LEADER,
+  VOICE_MUTE,
 };
 
 // Tap dancing
@@ -35,34 +41,44 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_SFT_QUES] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_QUES),
 };
 
+// combos
+const uint16_t PROGMEM lctl_spc[] = { MT(MOD_LCTL, KC_ESC), KC_SPC, COMBO_END };
+
+combo_t key_combos[COMBO_COUNT] = {
+  COMBO(lctl_spc, KC_ENTER),
+};
+
+bool get_combo_must_tap(uint16_t index, combo_t *combo) {
+  return true;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINUS,
+     KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_PSCR,      TMUX_LEADER,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_SFT_QUES),
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_PSCR,      TMUX_LEADER,  KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_SFT_QUES),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI,  LOWER,   KC_SPC,                    KC_ENTER, KC_BSPC, RAISE
+                                    KC_LGUI,  LOWER,   KC_SPC,                    KC_ENTER, RAISE,  KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_PSCR, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            QK_BOOT, KC_AMPR, KC_ASTR, KC_LBRC,  KC_RBRC,  KC_EQL,
+     KC_PSCR, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_END, KC_AMPR, KC_ASTR, KC_LBRC,  KC_RBRC, KC_MINUS, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_Y,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+     CMD_TAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_Y,                               U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,   KC_EQL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     CMD_TAB,  _______, KC_LEFT, KC_DEL, KC_UP,   KC_LBRC,                            KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,  KC_RIGHT, KC_PIPE,
+     TMUX_LEADER,  _______, KC_LEFT, KC_DEL, KC_UP,   KC_LBRC,                            KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT, KC_RIGHT, KC_PIPE,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, _______, _______, _______, KC_DOWN, KC_LCBR, KC_LPRN,          KC_RPRN,   KC_N,   KC_M,   KC_P2,   KC_P3,   KC_MINS, _______,
+     KC_LSFT, _______, VOICE_MUTE, _______, KC_DOWN, KC_LCBR, KC_LPRN,          KC_RPRN,   KC_N,  KC_PGDN, KC_PGUP,  KC_P3,   KC_MINS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, KC_DEL,                    SELWORD, KC_COPY, KC_PASTE
+                                     KC_LGUI, _______, KC_DEL,                    KC_COPY, SELWORD, KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -72,11 +88,11 @@ MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPRV,  KC_S,   KC_VOLU, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, KC_BSLS,
+     RGB_MOD, KC_MPRV,  KC_S,   KC_VOLU, KC_PGUP, KC_UNDS,                            KC_MS_L,  KC_MS_D, KC_MS_U, KC_MS_R, RGB_VAI, KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LALT, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN,          _______,  KC_N, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
+     KC_LALT, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN,  KC_B,    KC_LPRN,          CG_TOGG, KC_BTN1, KC_BTN3, KC_BTN2, RGB_SAD, RGB_VAD, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_F13, TG(ADJUST),  _______,                   _______, _______, _______
+                                    KC_UNDS, TG(ADJUST), KC_PMNS,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -121,6 +137,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING(SS_LCTL("a"));
       } else {
         // when keycode TMUX_LEADER is released
+      }
+      break;
+    case VOICE_MUTE:
+      if (record->event.pressed) {
+        // when keycode VOICE_MUTE is pressed
+        SEND_STRING(SS_LCTL("M"));
+      } else {
+        // when keycode VOICE_MUTE is released
       }
       break;
     case LOWER:
