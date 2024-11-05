7b6a96cba1081b860e0fd0d52b1c0e7b4f6bd081
mdube99 <mjdube99@gmail.com>
2024-11-05 09:49:14 -0600

    updated

--- keyboards/keebio/iris/keymaps/mdube_rev7/keymap.c
+++ keyboards/keebio/iris/keymaps/mdube_rev7/keymap.c
@@ -6,6 +6,18 @@
 #define _RAISE 2
 #define _GAME 3
 
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RCTL_T(KC_SCLN)
+
 enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     LOWER,
@@ -24,7 +36,7 @@ enum {
 };
 
 // Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_SFT_QUES]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_QUES),
     [TD_LALT_RCTL] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RCTL), // helpful for xfreerdp
@@ -38,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
              KC_TAB,   KC_Q,    KC_W,   KC_E,     KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSLS,
         // ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-             MT(  MOD_LCTL,    KC_ESC),   KC_A,     KC_S,    KC_D,                             KC_F,    KC_G,    KC_H,    KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT,
+             MT(  MOD_LCTL,    KC_ESC), HOME_A, HOME_S, HOME_D, HOME_F,    KC_G,                KC_H,    HOME_J,    HOME_K,   HOME_L, HOME_SCLN, KC_QUOT,
         // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
              KC_LSFT,   KC_Z,    KC_X,   KC_C,     KC_V,    KC_B,                              KC_PSCR,    _______,    KC_N,    KC_M,    KC_COMM,   KC_DOT, KC_SLSH, TD(TD_SFT_QUES),
         // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
